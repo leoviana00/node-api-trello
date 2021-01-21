b2e914d415f5dea4697b6c38293cbe85ba72eb52
Leonardo <leonardo.viana@armateus.com.br>
2021-01-21 19:49:18 -0300

    D - criando uma nova rota

--- index.js
+++ index.js
@@ -56,6 +56,19 @@ app.use('/tarefas', async (req, res) => {
   // res.send(resp);
 });
 
+app.get('/cards', async (req, res) => {
+
+  let lists = await fetch(`https://api.trello.com/1/boards/ojEiqbeQ/cards?key=${key}&token=${token}`);
+  let list = await lists.json();
+
+  list.forEach((id) => {
+  list[id.name] = [ id.due, id.labels, id.shortLink ]
+  });
+  console.log(list);
+  res.render('cards', {list: list});
+
+});
+
 
 app.listen(port, (erro) => {
   if(erro){
